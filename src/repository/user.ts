c8d257cb179b9b0658f2a7ebe85acbc397e8c446
Abegunde Precious Olanrewaju <preciousolanrewaju1998@gmail.com>
2024-08-02 20:58:23 +0100

    Update user.ts

--- src/repository/user.ts
+++ src/repository/user.ts
@@ -1,4 +1,4 @@
-import User from '../models/User';
+import User from '../models/user';
 import ErrorResponse from '../utils/errorResponse';
 import { IUserData } from '../interface';
 import { Op } from 'sequelize';
